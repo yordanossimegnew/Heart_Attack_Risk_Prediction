f325cbd17433250c2b49198bee9e2f41a3aa2416
yordanossimegnew <yordaanossimegnew@gmial.com>
2024-08-19 12:18:42 +0300

    updated config file

--- Scripts/config.py
+++ Scripts/config.py
@@ -1,5 +1,5 @@
 TARGET = "output"
-DATASET_PATH = "C:\\Users\\yozil\\Desktop\\My projects\\10. End_to_End_Heart_Attack_Risk_Prediction\\data\\raw data\\heart.csv"
+DATASET_PATH = "C:\\Users\\yozil\\Desktop\\My projects\\10. End_to_End_Heart_Attack_Risk_Prediction\\Scripts\\data\\raw data\\heart.csv"
 
 NUM_RECIPROCAL = ["trtbps"]
 NUM_YEO_JOHNSON = ["chol"]
